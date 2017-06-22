05cdeff113156acf34dc7a3aed474a120d78fb07
Marijn Faber <faber5@live.nl>
2017-06-22 11:09:19 +0200

    youri

--- muziekieApp/muziekieApp/Program.cs
+++ muziekieApp/muziekieApp/Program.cs
@@ -17,6 +17,7 @@ namespace muziekieApp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //dit is een test.
         }
     }
 }
